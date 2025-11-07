07f4341fc7eccff76b340dc0584f4df009ea4e0f
marcella <martins-marcella@hotmail.com>
2025-11-07 10:39:20 -0300

    add ajustes

--- src/domain/Produto.ts
+++ src/domain/Produto.ts
@@ -1,34 +1,40 @@
 export type TProduto = {
   id: string
   nome: string
-  quantidade: number
-  data: number
+  rendimento: number
+  dateTime: number
   valorUnitario: number
   valorTotal: number
   valorLucro: number
   valorGasto: number
+  valorVenda: number
+  descricao?: string
 }
 
 export class Produto {
   readonly nome: string
-  readonly quantidade: number
-  readonly data: number
+  readonly rendimento: number
+  readonly dateTime: number
   readonly valorUnitario: number
   readonly valorTotal: number
   readonly valorLucro: number
   readonly valorGasto: number
+  readonly valorVenda: number
+  readonly descricao?: string
 
   private constructor(data: TProduto) {
     this.nome = data.nome
-    this.quantidade = data.quantidade
-    this.data = data.data
+    this.rendimento = data.rendimento
+    this.dateTime = data.dateTime
     this.valorUnitario = data.valorUnitario
     this.valorTotal = data.valorTotal
     this.valorLucro = data.valorLucro
     this.valorGasto = data.valorGasto
+    this.valorVenda = data.valorVenda
+    this.descricao = data.descricao
   }
 
-  static create(data: TProduto) {
+  static create(data: TProduto): Produto {
     return new Produto(data)
   }
 }
--- src/domain/Venda.ts
+++ src/domain/Venda.ts
@@ -3,29 +3,40 @@ import { Produto } from './Produto'
 export type TVendedor = {
   id: string
   nome: string
-  produtos: { nome: string; quantidade: number }
-  valorReceber: number
-  valorRecebido: number
+  quantidade: number
+  total?: number
 }[]
 
 export type TVenda = {
   id: string
-  data: number
+  dateTime: number
+  valorGasto: number
   valorTotal: number
   valorLucro: number
-  valorGasto: number
   produtos: Produto[]
   vendedores: TVendedor[]
 }
 
 export class Venda {
+ readonly id: string
+  readonly dateTime: number
+  readonly valorGasto: number
+  readonly valorTotal: number
+  readonly valorLucro: number
+  readonly produtos: Produto[]
   readonly vendedores: TVendedor[]
 
   private constructor(data: TVenda) {
+    this.id = data.id
+    this.dateTime = data.dateTime
+    this.valorGasto = data.valorGasto
+    this.valorTotal = data.valorTotal
+    this.valorLucro = data.valorLucro
+    this.produtos = data.produtos
     this.vendedores = data.vendedores
   }
 
-  static create(data: TVenda) {
+  static create(data: TVenda): Venda {
     return new Venda(data)
   }
 }
